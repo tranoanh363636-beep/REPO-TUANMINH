85af9cc65821f3f0ddb2f26cc105035d80f2bd90
Minh <tranoanh363636@gmail.com>
2025-12-10 22:43:24 +0700

    add file

---
+++ REPO-TUANMINH/baitap1/1.py
@@ -0,0 +1,10 @@
+#Bai 1 
+ten="NguyenTuanMinh"
+tuoi="14"
+sothich="choicovua"
+monhocyeuthich="toan"
+print("ten:",ten )
+print("tuoi:",tuoi)
+print("sothich:",sothich)
+print("monhocyeuthich:",monhocyeuthich)
+
