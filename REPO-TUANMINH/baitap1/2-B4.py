3212fb73e92ec1595866173a54df6beaf2a641cd
Minh <tranoanh363636@gmail.com>
2025-12-16 20:51:10 +0700

    Add multipe files

---
+++ REPO-TUANMINH/baitap1/2-B4.py
@@ -0,0 +1,6 @@
+import math 
+r=float(input("bankinh cua hinh tron la:"))
+C=2 * math.pi * r
+S=math.pi * r**2
+print("chu vi cua hinh tron la:",C)
+print("Dien tich hinh tron laf:",S)
---
+++ REPO-TUANMINH/baitap1/6-B4.py
@@ -0,0 +1,7 @@
+a=int(input())
+hangdonvi=a%10
+hangchuc=(a//10)%10
+hangtram=a//100
+tong=hangdonvi+hangchuc+hangtram
+print(tong)
+    
