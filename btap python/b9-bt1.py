5d697b055bd23cb2d2bb019b78dcff89b4ed5708
Minh <tranoanh363636@gmail.com>
2025-12-25 20:46:44 +0700

    Add Multiple files

---
+++ btap python/b9-bt1.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    for a in range (9):
+        print("*",end=" ")
+    print( )    
---
+++ btap python/b9-bt3.py
@@ -0,0 +1,6 @@
+for i in range(6,1,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()    
+        
+    
